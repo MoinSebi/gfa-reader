eda5fe3f8f8e9fbc5aacd3b081c5401ea437d46d
moinSebi <s.vorbrugg@googlemail.com>
2021-09-10 12:24:41 +0200

    Added file checker and bugfix

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
-use std::hash::Hash;
+use std::path::Path as file_path;
 
 #[cfg(test)]
 
@@ -45,9 +45,9 @@ pub struct Gfa{
     pub edges: Vec<Edge>,
 }
 
-impl Gfa{
-    pub fn new() -> Self{
-        let nodes: HashMap<String, Node> = Hash::new();
+impl Gfa {
+    pub fn new() -> Self {
+        let nodes: HashMap<String, Node> = HashMap::new();
         let paths: Vec<Path> = Vec::new();
         let edges: Vec<Edge> = Vec::new();
         Self {
@@ -57,35 +57,33 @@ impl Gfa{
         }
     }
 
-    pub fn read_file(& mut self, file_name: &str){
-        let file = File::open(file_name).expect("ERROR: CAN NOT READ FILE\n");
-        let reader = BufReader::new(file);
+    pub fn read_file(&mut self, file_name: &str) {
+        if file_path::new(file_name).exists() {
+            let file = File::open(file_name).expect("ERROR: CAN NOT READ FILE\n");
+            let reader = BufReader::new(file);
 
-        // Iterate over lines
-        for line in reader.lines() {
-            let l = line.unwrap();
-            let line_split: Vec<&str> = l.split("\t").collect();
-            if l.starts_with("S") {
-                if self.nodes.contains_key(&String::from(line_split[1])){
-                    eprintln!("Warning: Duplicated node id found");
+            // Iterate over lines
+            for line in reader.lines() {
+                let l = line.unwrap();
+                let line_split: Vec<&str> = l.split("\t").collect();
+                if l.starts_with("S") {
+                    if self.nodes.contains_key(&String::from(line_split[1])) {
+                        eprintln!("Warning: Duplicated node id found");
+                    }
+                    self.nodes.insert(String::from(line_split[1]), Node { id: String::from(line_split[1]), seq: String::from(line_split[2]), len: line_split[2].len() });
+                } else if l.starts_with("P") {
+                    let name: String = String::from(line_split[1]);
+                    let dirs: Vec<bool> = line_split[2].split(",").map(|d| if &d[d.len() - 1..] == "+" { !false } else { !true }).collect();
+                    let nodd: Vec<String> = line_split[2].split(",").map(|d| d[..d.len() - 1].parse().unwrap()).collect();
+                    self.paths.push(Path { name: name, dir: dirs, nodes: nodd });
+                } else if l.starts_with("L") {
+                    self.edges.push(Edge { from: line_split[1].parse().unwrap(), to: line_split[3].parse().unwrap(), from_dir: if line_split[2] == "+" { !false } else { !true }, to_dir: if line_split[4] == "+" { !false } else { !true } })
                 }
-                self.nodes.insert(String::from(line_split[1]), Node { id: String::from(line_split[1]), seq: String::from(line_split[2]), len: line_split[2].len() });
-
             }
-            else if l.starts_with("P"){
-                let name: String = String::from(line_split[1]);
-                let dirs: Vec<bool> = line_split[2].split(",").map(|d| if &d[d.len()-1..] == "+" { !false } else { !true }).collect();
-                let nodd: Vec<String> = line_split[2].split(",").map(|d| d[..d.len()-1].parse().unwrap()).collect();
-                paths.push(Path {name: name, dir: dirs, nodes: nodd});
-            }
-            else if l.starts_with("L") {
-                edges.push(Edge{from: line_split[1].parse().unwrap() , to: line_split[3].parse().unwrap() , from_dir: if line_split[2] == "+" { !false } else { !true }, to_dir: if line_split[4] == "+" { !false } else { !true }})
-
-            }
-
         }
     }
 }
+
 
 #[cfg(test)]
 mod tests {
